17855f996f8836959b869deb7819a7b922f51e22
Vivek Pramanik <39510579+icodervivek@users.noreply.github.com>
2025-06-15 14:39:12 +0530

    update loops and function

---
+++ step1/lec1/funcpassbyvalueandref.cpp
@@ -0,0 +1,21 @@
+/**************** Hey There, Vivek Here ****************/
+/*                Dream Big & Compile Them            */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printName(string &s)
+{
+    s = "shyam";
+    cout << "Hey, " << s << endl;
+}
+
+int main()
+{
+    string s = "raghu";
+    printName(s);
+    cout << "Hey, " << s;
+
+    return 0;
+}
---
+++ step1/lec1/while.cpp
@@ -0,0 +1,26 @@
+/**************** Hey There, Vivek Here ****************/
+/*                Dream Big & Compile Them            */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
